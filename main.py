4ceb71318968eac66eeed72331ec852112d64e97
mtali <emmanuelmtali@yahoo.com>
2024-11-02 02:06:04 +0300

    Add ship to game screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -11,6 +12,7 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -19,6 +21,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 
