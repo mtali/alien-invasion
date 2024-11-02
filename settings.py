ad401ac12d9f45a0d0aee4ab218928185dc503f2
mtali <emmanuelmtali@yahoo.com>
2024-11-02 14:23:23 +0300

    Implement full screen

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = 0  # will be overriden
+        self.screen_height = 0  # will be overriden
         self.bg_color = (230, 230, 230)
         self.speed = 1.5
