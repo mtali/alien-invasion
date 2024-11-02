9b90cdcb64465363896f12d4d6b74375d412b2be
mtali <emmanuelmtali@yahoo.com>
2024-11-02 15:37:29 +0300

    Create a complete alien fleet

--- main.py
+++ main.py
@@ -67,21 +67,27 @@ class AlienInvasion:
             if bullet.rect.bottom < 0:
                 self.bullets.remove(bullet)
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
-        alient_width = alien.rect.width
+        alient_width, alien_height = alien.rect.size
         alien.x = alient_width + 2 * alient_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _create_fleet(self):
         alien = Alien(self)
-        alient_width = alien.rect.width
-        available_space = self.settings.screen_width - (2 * alient_width)
-        number_aliens_x = available_space // (2 * alient_width)
+        alient_width, alien_height = alien.rect.size
 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        available_space_x = self.settings.screen_width - (2 * alient_width)
+        number_aliens_x = available_space_x // (2 * alient_width)
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
     def run_game(self):
         while True:
