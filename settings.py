19f3eaad2d8798412ec5300e268048f1d4dd0820
mtali <emmanuelmtali@yahoo.com>
2024-11-02 14:26:43 +0300

    Add default bullet settings

--- settings.py
+++ settings.py
@@ -1,6 +1,15 @@
 class Settings:
     def __init__(self):
+        # Game
         self.screen_width = 0  # will be overriden
         self.screen_height = 0  # will be overriden
         self.bg_color = (230, 230, 230)
+
+        # Ship
         self.speed = 1.5
+
+        # Bullets
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
